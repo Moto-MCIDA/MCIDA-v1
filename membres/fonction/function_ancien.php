cff8d14307f585fc7799c757e2f2ddd68666ce91
Kylian Grandy <118520201+Muuushuuu@users.noreply.github.com>
2025-07-12 19:34:53 +0200

    revert: Back in good base

--- membres/fonction/function_ancien.php
+++ membres/fonction/function_ancien.php
@@ -1,148 +1,149 @@
 <?php
-function ancien_balade($db){
-	date_default_timezone_set('Europe/Paris');
-    $ajrd = date("Y-m-d");
-	$booleen = $db->query("SELECT count(*) FROM balade WHERE date_sortie <= '$ajrd'");
-	$bool = $booleen->fetchColumn();
-	if($bool >= 1){
-		$balade = $db->prepare("SELECT * FROM balade WHERE date_sortie <= '$ajrd' ORDER BY date_sortie DESC");
-		$balade->execute();
-		while ($res = $balade->fetch(PDO::FETCH_OBJ)) {
-			affiche_ancien_balade($db,$res->id, $res->nom, $res->date_sortie, $res->id_createur, $res->description);
+//////////////////////////////////////////////////////////////////////////
+//																		//
+// 	    			   Fonction ancien - Prive		 			   	    //
+//																		//
+//////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************************************************************************** */
+// fonction ancien_balade avec un parametre qui est la variable de connexion a la db 
+// cette fonction va recuperer toutes balades dont la date est <= a celle du jour 
+// puis elle va faire une boucle ligne par ligne afin executer la fonction affiche_ancien_balade
+/******************************************************************************************************************************************* */
+	function ancien_balade($db){
+		date_default_timezone_set('Europe/Paris');
+    	$ajrd = date("Y-m-d"); // on recupere la date du jour dans une variable
+		$booleen = $db->query("SELECT count(*) FROM balade WHERE date_sortie <= '$ajrd'"); // on compte combien de balade on une date <= a la date du jour 
+		$bool = $booleen->fetchColumn();
+		if($bool >= 1){ // si il y a plus de 1 balade 
+			$balade = $db->prepare("SELECT * FROM balade WHERE date_sortie <= '$ajrd' ORDER BY date_sortie DESC"); // on recuper tous les infos de toutes les balades concerner (trier par date decroissant)
+			$balade->execute();
+			while ($res = $balade->fetch(PDO::FETCH_OBJ)) { // on fait une boucle afin d'utiliser une ligne du resultat precedent qui est sous forme de tableau
+				affiche_ancien_balade($db,$res->id, $res->nom, $res->date_sortie, $res->id_createur, $res->description); // execute la fonction avec affiche_ancien_balade avec toutes les info sur la balade en parametre
+			}
+		}else{
+			// si il a 0 balade on affiche un message
+			echo 'pas d\'ancienne balade';
 		}
-	}else{
-		echo 'pas d\'ancienne balade';
+    	
 	}
-}
-
-function affiche_ancien_balade($db, $id, $nom, $date_sortie, $id_createur, $description){
-	$sql = "SELECT email, prenom, nom FROM utilisateur WHERE id = $id_createur";
-    $select = $db->query($sql);
-    $res = $select->fetch();
-	$html = '<tr>';
-	$html .= '<td>';
-	$html .= '<a href="message/message_groupe_balade.php?id='.$id.'">';
-	$html .= '<div id="ancien_balade">';
-	$html .= '<h5 style="display: flex;flex-direction: column;justify-content: center;">'.$nom.'</h5>';
-	$html .= '<h6>Du : '.$date_sortie.'</h6>';
-	$html .= '<h6>Proposé par : '.$res['nom'].' '.$res['prenom'].'</h6>';
-	$html .= '<div id="image_bloc">';
-	$html .= '<div id="img_ab">';
-	$chem_img = 'document/photo_balade/'.$id;
-	if (is_dir($chem_img)) {
-		$handle  = opendir($chem_img);
-		while ($file = readdir($handle)) {
-		  if(preg_match ("!(\.jpg|\.jpeg)$!i", $file)) {
-		    $listef[] = $file;
-		  }
+/******************************************************************************************************************************************** */
+// fonction affiche_ancien_balade avec comme parametre la variable de connexion a la db  et toutes les infos de la balade 
+// dans cette fonction on recupere des info sur la personne qui la cree 
+// puis on cree une variable html qui contiendra du code html (partie du tableau) avec a l'interieur les différentes variable de la sortie et de l'utilisateur
+// il y a un bloc photo avec la photo de profil du créateur, ou les photos de la sortie qui defile aléatoirement (si il en a)
+/******************************************************************************************************************************************** */
+	function affiche_ancien_balade($db, $id, $nom, $date_sortie, $id_createur, $description){
+		// recuperation des informations sur l'utilisateur (email, nom, prenom) 
+		$sql = "SELECT email, prenom, nom FROM utilisateur WHERE id = $id_createur";
+	    $select = $db->query($sql);
+	    $res = $select->fetch();
+		// on ajoute du code html et les différentes variables 
+		$html = '<tr>';
+		$html .= '<td>';
+		$html .= '<a href=\'message/message_groupe_balade.php?id='.$id.'\'>';
+		$html .= '<div id="ancien_balade">';
+		$html .= '<h5 style="display: flex;flex-direction: column;justify-content: center;">'.$nom.'</h5>';
+		$html .= '<h6>Du : '.$date_sortie.'</h6>';
+		$html .= '<h6>Proposé par : '.$res['nom'].' '.$res['prenom'].'</h6>';
+		// partie bloc photo
+		$html .= '<div id="image_bloc">';
+		$html .= '<div id="img_ab">';
+    	//On indique le dossier images
+		$chem_img = 'document/photo_balade/'.$id;
+		if (is_dir($chem_img)) {
+			//On ouvre le dossier images
+			$handle  = opendir($chem_img);
+			//On parcoure chaque élément du dossier
+			while ($file = readdir($handle))
+			{
+			  //Si les fichiers sont des images
+			  if(preg_match ("!(\.jpg|\.jpeg)$!i", $file))
+			    {
+			      $listef[] = $file;
+			    }
+			}
+			//permet de prendre une image totalement au hasard (RANDom) parmi toutes les images trouvées.
+			$html .= "<img id=\"photo_reload\" src=\"".$chem_img."/".$listef[rand(0, count($listef)-1)]."\"/>";
+		}else{
+			// si le dossier image existe pas on mets la photo de profil du créateur 
+			$html .= '<img src="image/utilisateurs/'.$res['email'].'/'.$res['email'].'_profil.jpg">';
 		}
-		$html .= "<img id=\"photo_reload\" src=\"".$chem_img."/".$listef[rand(0, count($listef)-1)]."\"/>";
-	}else{
-		$html .= '<img src="image/utilisateurs/'.$res['email'].'/'.$res['email'].'_profil.jpg">';
+		$html .= '</div>';
+		$html .= '</div>';
+		$html .= '</div>';
+		$html .= '</a>';
+		$html .= '</td>';
+		$html .= '</tr>';
+		print($html);	// on affiche la variable 				
 	}
-	$html .= '</div>';
-	$html .= '</div>';
-	$html .= '</div>';
-	$html .= '</a>';
-	$html .= '</td>';
-	$html .= '</tr>';
-	print($html);				
-}
+/******************************************************************************************************************************************** */
+// fonction recup_nom_date avec comme parametre a variable de connexion a la db et l'id de la balade 
+// sert a recuperer le nom et la date de sortie de la sortie 
+/******************************************************************************************************************************************** */
+	function recup_nom_date($id_groupe, $db){
+	    $sql = "SELECT nom, date_sortie FROM balade WHERE id = $id_groupe";
+	    $select = $db->query($sql);
+	    $res = $select->fetch();
+	    return $res;
+	}
+/******************************************************************************************************************************************** */
+// fonction affiche_nom avec comme parametre un tableau (avec dedans le nom et la date de la balade)
+// sert a afficher dans une div les info de la sortie
+/******************************************************************************************************************************************** */
+	function affiche_nom($np_groupe){
+	    $nom = $np_groupe['nom'];
+	    $date = $np_groupe['date_sortie'];
+	    $html = "<div id='nom_bar'><h1 id = 'nom'> $nom du $date</h1></div>";
+	    echo $html;
+	}
+/******************************************************************************************************************************************** */
+// fonction recup_message avec comme parametre a variable de connexion a la db et l'id de la balade 
+// sert recuperer les messages de la balade
+/******************************************************************************************************************************************** */
+	function recup_message($id_groupe, $db){
+	    $sql = "SELECT * FROM `message_groupe` WHERE id_balade = $id_groupe";
+	    $select = $db->query($sql);
+	    return $select;
+	}
+/******************************************************************************************************************************************** */
+// fonction affiche_message avec comme parametre un tableau avec les infos des messages, l'id de la balade, et avec la variable de connection a la db
+// dans cette fonction on va afficher sous forme de liste les messages
+// on mets l'id droit pour les li qui correspond au message de l'utilisateur, sinon on mets l'id gauche
+/******************************************************************************************************************************************** */
+	function affiche_message($messages, $id_groupe, $db){
+	    $id_usr = $_SESSION['user_id'];
+	    $dst = recup_nom_date($id_groupe, $db); // utilisation de la fonction precedente
+	    $html = "<ul id='ul'>";
+		// on fait une boucle for pour chaque message 
+	    foreach($messages as $key => $message){
+			// on recupere le mois et le jour
+	        $date_mess_mois = substr($message['date_heure'], 0, 2);
+	        $date_mess_jour = substr($message['date_heure'], 3, 2);
+	        if ($message['nb_photo'] != 0) {
+	    		$image_html = '<div id="image_bloc_msg">';
+        		//for ($i = 0; $i < $message['nb_photo']; $i++) {
+        			$image_html .= '<a href="affiche_photo.php?idg='.$id_groupe.'&msgdh='.$message['date_heure'].'&msgiu='.$message['id_utilisateur'].'&nbp='.$message['nb_photo'].'"><img src="document/photo_balade/'.$id_groupe.'/'.$message['date_heure'].'_'.$message['id_utilisateur'].'_0.jpg"></a>';
+        			$image_html .= '<a href="affiche_photo.php?idg='.$id_groupe.'&msgdh='.$message['date_heure'].'&msgiu='.$message['id_utilisateur'].'&nbp='.$message['nb_photo'].'"><p>Nombre de photo : '.$message['nb_photo'].'</p></a>';
+        		//}
+        		$image_html .= '</div>';
+        	}else{
+        		$image_html = '';
+        	}
 
+	        if($message["id_utilisateur"] == $id_usr){
+	            $html .= "<li id = 'droite'> <strong>Moi le $date_mess_jour / $date_mess_mois :</strong> $message[message] <br> $image_html </li>";
+	        }
+	        else{
+	            $id_utilisateur = $message["id_utilisateur"];
+	            $sql_profil = "SELECT * FROM `utilisateur` WHERE id = $id_utilisateur";
+	            $profil = $db->query($sql_profil);
+	            $res_profil = $profil->fetch();
+	            $html .= "<li id = 'gauche'> <strong>$res_profil[nom] $res_profil[prenom] le $date_mess_jour / $date_mess_mois :</strong> $message[message] <br> $image_html </li>";
+	        }
+	    }
+	    $html .= "</ul>";
+	    echo $html;
+	}
 
-function ancienne_balade_info($db, $id_balade) {
-    $sql = "SELECT * FROM balade WHERE id = ?";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([$id_balade]);
-    $balade = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($balade) {
-        $createur = recup_nom_utilisateur($db, $balade['id_createur']);
-
-        echo "<div id='information'>";
-        echo "<h2>" . htmlspecialchars($balade['nom']) . "</h2>";
-
-        echo "<div class='info-section'>";
-        echo "<strong>Date :</strong> " . htmlspecialchars($balade['date_sortie']);
-        echo "</div>";
-
-        echo "<div class='info-section'>";
-        echo "<strong>Description :</strong><br>" . nl2br(htmlspecialchars($balade['description']));
-        echo "</div>";
-
-        echo "<div class='info-section'>";
-        echo "<strong>Créée par :</strong> " . htmlspecialchars($createur);
-        echo "</div>";
-
-        // // Documents attachés
-        // $stmt_docs = $db->prepare("SELECT * FROM balade WHERE id_balade = ?");
-        // $stmt_docs->execute([$id_balade]);
-        // $docs = $stmt_docs->fetchAll(PDO::FETCH_ASSOC);
-        // if ($docs) {
-        //     echo "<div class='info-section'>";
-        //     echo "<strong>Documents :</strong><br>";
-        //     foreach ($docs as $doc) {
-        //         echo "<a href='" . htmlspecialchars($doc['chemin']) . "' target='_blank' id='document'>📎 " . htmlspecialchars($doc['nom']) . "</a><br>";
-        //     }
-        //     echo "</div>";
-        // }
-
-        echo "<a href='message_groupe.php?id=" . $id_balade . "'><div id='boutton'>Accéder au groupe</div></a>";
-
-        echo "<h2 style='margin-top: 30px;'>Participants</h2>";
-        echo "<div id='participants-list'>";
-
-        $stmt_participants = $db->prepare("SELECT utilisateur.nom, utilisateur.prenom, utilisateur.email, utilisateur.id FROM balade_membre JOIN utilisateur ON utilisateur.id = balade_membre.id_utilisateur WHERE id_balade = ?");
-        $stmt_participants->execute([$id_balade]);
-
-        while ($row = $stmt_participants->fetch(PDO::FETCH_ASSOC)) {
-            $img_path = 'img/photos_profil/' . strtolower($row['prenom']) . '.png';
-            if (!file_exists($img_path)) {
-                $img_path = 'img/default.png';
-            }
-
-            echo "<div class='participant-card'>";
-            echo "<img src='" . $img_path . "' alt='photo' >";
-            echo "<span> ". htmlspecialchars($row['prenom'] . " " . $row['nom']) ." </span>";
-            echo "</div>";
-        }
-
-        echo "</div>"; // end #participants-list
-
-
-        echo "</div>"; // end #information
-    } else {
-        echo "<p>❌ Balade introuvable.</p>";
-    }
-}
-
-function recup_nom_utilisateur($db, $id_utilisateur) {
-    $sql = "SELECT nom, prenom FROM utilisateur WHERE id = ?";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([$id_utilisateur]);
-    $res = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $res ? ($res['nom'] . ' ' . $res['prenom']) : 'Inconnu';
-}
-
-function affiche_reponses_participants($db, $id_balade) {
-    echo "<div class='info-section'>";
-    echo "<strong>Réponses complémentaires des participants :</strong><br>";
-
-    $sql = "SELECT u.prenom, u.nom, br.reponse FROM balade_reponses br JOIN utilisateur u ON u.id = br.id_utilisateur WHERE br.id_balade = ?";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([$id_balade]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    if ($res) {
-        echo "<ul>";
-        foreach ($res as $r) {
-            echo "<li><strong>" . htmlspecialchars($r['prenom'] . ' ' . $r['nom']) . " :</strong> " . htmlspecialchars($r['reponse']) . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "Aucune réponse enregistrée.";
-    }
-
-    echo "</div>";
-}
 ?>
--- membres/message/message_groupe_balade.php
+++ membres/message/message_groupe_balade.php
@@ -1,8 +1,8 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////
-//                                                                                  //
-//                                     PHP                                          //
-//                                                                                  //
+//																					//
+//										 PHP 										//
+//                                                                                  // 
 //////////////////////////////////////////////////////////////////////////////////////
 
 // Include les différentes pages PHP (fonction)
@@ -11,11 +11,10 @@ include("../../fonction/connexion_db.php");
 include("../fonction/function_message.php");
 include("../fonction/function_notification.php");
 include("../../fonction/fonction_email.php");
-include("../fonction/function_ancien.php"); // ajout de l'import pour les infos balade
 
-// Vérification qu'on a bien une session, qu'on est bien connecté sinon redirection page 'connexion.php'
+// Vérification qu'on a bien une session, qu'on est bien connecter sinon redirection page 'connexion.php'
 if(isset($_SESSION['user_id']) AND !empty($_SESSION['user_id'])) {
-
+	
 $user_id = $_SESSION['user_id'];
 if(isset($_GET['id']) AND !empty($_GET['id'])) {
 
@@ -24,9 +23,10 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 	$date = (new DateTime($dateFr))->format("Y-m-d H:i:s");
 	$id_link = $_GET['id'];
 
-	$booleen = $db->query("SELECT count(*) FROM ligne WHERE user = '$user_id' AND link ='message groupe actif' AND id_link='$id_link'");
+	$booleen = $db->query("SELECT count(*) FROM ligne WHERE user = '$user_id' AND link ='message groupe actif' AND id_link='$id_link'"); // on compte combien de balade on une date >= a la date du jour 
 	$bool = $booleen->fetchColumn();
 	if($bool >= 1){
+		// UPDATE `balade_membre` SET reponse='$choix_reponse', commentaire='$commentaire', nb_prs = '$nb_personne' WHERE id_utilisateur=$user_id AND id_balade=$balade_id"
 		$sql = "UPDATE `ligne` SET date_ligne='$date' WHERE user = '$user_id' AND link = 'message groupe actif' AND id_link='$id_link'";
 	}else{
 		$sql = "INSERT INTO `ligne` (user, link, id_link, date_ligne) VALUES ('$user_id', 'message groupe actif', '$id_link', '$date')";
@@ -34,6 +34,11 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 	$ajout_ligne = $db->query($sql);
 }
 ?>
+<!-- ---------------------------------------------------------------------------------- -->
+<!--										 	 										-->
+<!--										 HTML 										-->
+<!--										 	 										-->
+<!-- ---------------------------------------------------------------------------------- -->   
 
 <!DOCTYPE html>
 <html>
@@ -51,52 +56,85 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 	<title>MCIDA | <?php echo $_SESSION['user_nom'].' '.$_SESSION['user_prenom']; ?></title>
 </head>
 <body>
-	<?php include_once('../fonction/header.php'); ?>
+	<?php 
+	include_once('../fonction/header.php');
+	?>
+	
+
 
 	<div id="information">
 		<?php 
 			$id_groupe = $_GET['id'];
-			// Affiche les infos détaillées de la balade passée (titre, description, créateur, etc.)
-			echoblock:
-			echo "<div class='details_balade'>";
-			ancienne_balade_info($db, $id_groupe); // Fonction définie dans function_ancien.php
-			echo "</div>";
+			blocInfoBalade($id_groupe, $db);
 		?>
+
 
 		<?php 
-			$balade = $db->prepare("SELECT date_sortie FROM balade WHERE id='$id_groupe'");
-			$balade->execute();
-			$res = $balade->fetch();
+		$id_groupe = $_GET['id'];
+		$balade = $db->prepare("SELECT date_sortie FROM balade WHERE id='$id_groupe'");
+		$balade->execute();
+		$res = $balade->fetch();
 
-			date_default_timezone_set('Europe/Paris');
-			$ajrd = date("Y-m-d");
+		date_default_timezone_set('Europe/Paris');
+		$ajrd = date("Y-m-d");
 
-			if ($res['date_sortie'] <= $ajrd) {
+		if ($res['date_sortie']<=$ajrd) {
 		?>
-			<div style="display: flex; flex-direction: row; justify-content: center; width: 95%; height: 30px; margin: auto; background-color: transparent; position: relative;">
-				<a href="download/download.php?id=<?php echo $id_groupe; ?>" style="color: white; background-color: #1D63EB; padding: 5px 10px; text-align: center; border-radius: 15px;">Afficher toutes les photos</a>
+			<div style="    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    grid-row-start: 2;
+    grid-column-start: 1;
+    grid-row-end: 3;
+    grid-column-end: 7;
+    width: 95%;
+    height: 30px;
+    margin: auto;
+    background-color: transparent;
+    position: relative;
+			">
+				<a href="download/download.php?id=<?php echo $id_groupe; ?>" style="  
+				color: white;
+				background-color: #1D63EB;
+				padding: 5px 10px;
+				text-align: center;
+				border-radius: 15px;">Afficher toutes les photos</a>
 			</div>
-			<script>document.querySelector(".navigation > ul > li:nth-child(2)").classList.add('active');</script>
+
+			<script>		
+				let active = document.querySelector(".navigation > ul > li:nth-child(2)");
+				active.classList.add('active');
+			</script>
+
 		<?php 
-			} else {
+		}else{
+			?>
+			<script>			
+				let active = document.querySelector(".navigation > ul > li:nth-child(1)");
+				active.classList.add('active');
+			</script>
+			<?php
+		}
 		?>
-			<script>document.querySelector(".navigation > ul > li:nth-child(1)").classList.add('active');</script>
-		<?php } ?>
 	</div>
 
+
+<!-- -------------------------------- Affichage des messages -------------------------------- -->	
 	<div id="message">
 		<div id="afficheMsg">
 			<div>
 				<?php 
-					affiche_nom_balade(recup_nom_balade($id_groupe, $db));
-					affiche_message_balade(recup_message_balade($id_groupe, $db), $id_groupe, $db);
+					$id_groupe = $_GET['id'];
+					affiche_nom_balade(recup_nom_balade($id_groupe,$db));
+					affiche_message_balade(recup_message_balade($id_groupe,$db), $id_groupe ,$db);
 				?>
 			</div>
 		</div>
-
+<!-- -------------------------------- Ecrire un message -------------------------------- -->
 		<form id="envoyerMsg" method="post" enctype="multipart/form-data">
 			<div>
-				<input id='bouton' onclick="backfunction()" type="submit" name="envoyerMsg" value="Envoyer" />
+				
+				<input id='bouton' onclick="backfunction()" type="submit"name="envoyerMsg" value="Envoyer" />
 			</div>
 			<div>
 				<p class="file-return"></p>
@@ -111,51 +149,66 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 		</form>
 	</div>
 </body>
-</html>
-
+</html>	
 <script type="text/javascript">
-	document.getElementById('ul')?.scrollTo(0, document.getElementById('ul').scrollHeight);
-	function updateDiv() {
-		$('#afficheMsg > div > ul').load(window.location.href + " #afficheMsg > div > ul > li" );
+
+		element_b = document.getElementById('ul'); // Ne Fonctionne Pas
+		element_b.scrollTop = element_b.scrollHeight;
+
+	function updateDiv()
+	{ 	
+	    $('#afficheMsg > div > ul').load(window.location.href + " #afficheMsg > div > ul > li" );
 	}
-	setInterval(updateDiv, 5000);
-
-	function updatenotif() {
-		$('#notif_img').load(window.location.href + " #notif_img > a" );
+	setInterval('updateDiv()', 5000);
+	function updatenotif()
+	{ 	
+	    $('#notif_img ').load(window.location.href + " #notif_img > a " );
 	}
-	setInterval(updatenotif, 5000);
+	setInterval('updatenotif()', 5000);
 
-	document.querySelector("html").classList.add('js');
-	var fileInput = document.querySelector(".input-file"),
-		button = document.querySelector(".input-file-trigger"),
-		the_return = document.querySelector(".file-return");
 
-	button.addEventListener("keydown", function(event) {
-		if (event.keyCode == 13 || event.keyCode == 32) {
-			fileInput.focus();
-		}
-	});
-	button.addEventListener("click", function(event) {
+
+// ajout de la classe JS à HTML
+document.querySelector("html").classList.add('js');
+
+// initialisation des variables
+var fileInput = document.querySelector( ".input-file" ),
+	button = document.querySelector( ".input-file-trigger" ),
+	the_return = document.querySelector(".file-return");
+
+// action lorsque la "barre d'espace" ou "Entrée" est pressée
+button.addEventListener( "keydown", function( event ) {
+	if ( event.keyCode == 13 || event.keyCode == 32 ) {
 		fileInput.focus();
-		return false;
-	});
-	fileInput.addEventListener("change", function(event) {
-		the_return.innerHTML = 'Document(s) séléctionné(s)';
-		the_return.style.display = 'flex';
-	});
-
-	function backfunction(){
-		var loading = $('#loading');
-		loading.addClass('yes');
-		loading.removeClass('no');
 	}
+});
+
+// action lorsque le label est cliqué
+button.addEventListener( "click", function( event ) {
+	fileInput.focus();
+	return false;
+});
+
+// affiche un retour visuel dès que input:file change
+fileInput.addEventListener( "change", function( event ) {
+	the_return.innerHTML = 'Document(s) séléctionnée(s)';
+	the_return.style.display = 'flex';
+});
+
+function backfunction(){
+	var loading = $('#loading');
+	loading.addClass('yes');
+	loading.removeClass('no');
+}
 </script>
+
+
 
 <?php
 if(isset($_POST['envoyerMsg'])){
 	envoye_message_balade($db);
 }
-} else {
-	header("Location: ../../connexion.php");
+}else{
+    header("Location: ../../connexion.php");
 }
 ?>
